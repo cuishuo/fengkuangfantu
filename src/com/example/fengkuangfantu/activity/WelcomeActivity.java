0224b04227688530f24544284d5362097576c2fd
tongmeng <xiaoding0606@163.com>
2015-09-21 22:34:39 +0800

    add the welcom activity

---
+++ src/com/example/fengkuangfantu/activity/WelcomeActivity.java
@@ -0,0 +1,14 @@
+package com.example.fengkuangfantu.activity;
+
+import android.os.Bundle;
+
+import com.example.fengkuangfantu.R;
+
+public class WelcomeActivity extends BaseActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_welcome);
+	}
+}
