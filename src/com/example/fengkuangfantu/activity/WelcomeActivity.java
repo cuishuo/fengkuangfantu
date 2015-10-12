8678a344b96374b2dc3e63c2c8d19eae2bbf8324
tongmeng <tongmeng@zmeng123.com>
2015-10-12 14:15:18 +0800

    add the welcom page

--- src/com/example/fengkuangfantu/activity/WelcomeActivity.java
+++ src/com/example/fengkuangfantu/activity/WelcomeActivity.java
@@ -20,48 +20,59 @@ public class WelcomeActivity extends BaseActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_welcome);
-		text1 = (TextView)findViewById(R.id.text1);
-		text2 = (TextView)findViewById(R.id.text2);
-		text3 = (TextView)findViewById(R.id.text3);
-		text4 = (TextView)findViewById(R.id.text4);
+//		text1 = (TextView)findViewById(R.id.text1);
+//		text2 = (TextView)findViewById(R.id.text2);
+//		text3 = (TextView)findViewById(R.id.text3);
+//		text4 = (TextView)findViewById(R.id.text4);
 		initViews();
 		
 	}
 	
 	private void initViews() {
-		Animation animation=AnimationUtils.loadAnimation(this, R.anim.xuanzhuan);//xuanzhuan
-//		Animation animation=AnimationUtils.loadAnimation(this, R.anim.actin);//����
-//		Animation animation=AnimationUtils.loadAnimation(this, R.anim.suofang);//suo fang
-		text1.startAnimation(animation);
-		animation.setFillAfter(true);
-		text2.startAnimation(animation);
-		animation.setFillAfter(true);
-		text3.startAnimation(animation);
-		animation.setFillAfter(true);
-		text4.startAnimation(animation);
-		animation.setFillAfter(true);
-		animation.setAnimationListener(new AnimationListener() {
+		mHandler.postDelayed(new Runnable() {
 			
 			@Override
-			public void onAnimationStart(Animation animation) {
-				// TODO Auto-generated method stub
-				
-			}
-			
-			@Override
-			public void onAnimationRepeat(Animation animation) {
-				// TODO Auto-generated method stub
-				
-			}
-			
-			@Override
-			public void onAnimationEnd(Animation animation) {
+			public void run() {
 				// TODO Auto-generated method stub
 				Intent intent = new Intent();
 				intent.setClass(getApplicationContext(), MainActivity.class);
 				startActivity(intent);
 				finish();
 			}
-		});
+		}, 2000);
+//		Animation animation=AnimationUtils.loadAnimation(this, R.anim.xuanzhuan);//xuanzhuan
+////		Animation animation=AnimationUtils.loadAnimation(this, R.anim.actin);//����
+////		Animation animation=AnimationUtils.loadAnimation(this, R.anim.suofang);//suo fang
+//		text1.startAnimation(animation);
+//		animation.setFillAfter(true);
+//		text2.startAnimation(animation);
+//		animation.setFillAfter(true);
+//		text3.startAnimation(animation);
+//		animation.setFillAfter(true);
+//		text4.startAnimation(animation);
+//		animation.setFillAfter(true);
+//		animation.setAnimationListener(new AnimationListener() {
+//			
+//			@Override
+//			public void onAnimationStart(Animation animation) {
+//				// TODO Auto-generated method stub
+//				
+//			}
+//			
+//			@Override
+//			public void onAnimationRepeat(Animation animation) {
+//				// TODO Auto-generated method stub
+//				
+//			}
+//			
+//			@Override
+//			public void onAnimationEnd(Animation animation) {
+//				// TODO Auto-generated method stub
+//				Intent intent = new Intent();
+//				intent.setClass(getApplicationContext(), MainActivity.class);
+//				startActivity(intent);
+//				finish();
+//			}
+//		});
 	}
 }
