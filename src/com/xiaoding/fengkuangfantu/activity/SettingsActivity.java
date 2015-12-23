7cd38725c1932ffafea27ced7d7d5029bd3794d7
cuishuo <cuishuo7012@163.com>
2015-12-23 11:32:01 +0800

    add title bar

--- src/com/xiaoding/fengkuangfantu/activity/SettingsActivity.java
+++ src/com/xiaoding/fengkuangfantu/activity/SettingsActivity.java
@@ -13,7 +13,11 @@ import android.preference.PreferenceActivity;
 import android.preference.PreferenceManager;
 import android.preference.SwitchPreference;
 import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.Window;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class SettingsActivity extends PreferenceActivity implements OnPreferenceClickListener {
 
@@ -22,13 +26,26 @@ public class SettingsActivity extends PreferenceActivity implements OnPreference
     private Preference mevaluatePreference;
     private Preference mupdateVersionPreference;
     private Preference maboutUsPreference;
-    
+    private TextView titleTextView;
+    private ImageView returnImageView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         // TODO Auto-generated method stub
-        super.onCreate(savedInstanceState);             
-        addPreferencesFromResource(R.xml.settings);  
+        super.onCreate(savedInstanceState);
         setContentView(R.layout.setting);
+        titleTextView = (TextView) findViewById(R.id.titleTextView);
+        titleTextView.setText(getResources().getString(R.string.settings));
+        returnImageView = (ImageView) findViewById(R.id.iv_back);
+        addPreferencesFromResource(R.xml.settings);
+        returnImageView.setOnClickListener(new OnClickListener() {
+            
+            @Override
+            public void onClick(View arg0) {
+                // TODO Auto-generated method stub
+                finish();
+            }
+        });
         initPreferences();
     }
 
--- src/com/xiaoding/fengkuangfantu/activity/SettingsFeedbackActivity.java
+++ src/com/xiaoding/fengkuangfantu/activity/SettingsFeedbackActivity.java
@@ -4,13 +4,30 @@ import com.xiaoding.fengkuangfantu.R;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class SettingsFeedbackActivity extends Activity {
+
+    private TextView titleTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         // TODO Auto-generated method stub
         super.onCreate(savedInstanceState);
         setContentView(R.layout.settings_feedback);
+        titleTextView = (TextView) findViewById(R.id.titleTextView);
+        titleTextView.setText(getResources().getString(R.string.settings_feedback));
+        ImageView returnImageView = (ImageView) findViewById(R.id.iv_back);
+        returnImageView.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+                // TODO Auto-generated method stub
+                finish();
+            }
+        });
     }
 }
