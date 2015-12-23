ff3c6a393beabd4efc4bce67352edb8ccf022f54
cuishuo <cuishuo7012@163.com>
2015-12-23 17:53:30 +0800

    modify feedback

--- src/com/xiaoding/fengkuangfantu/activity/FeedbackActivity.java
+++ src/com/xiaoding/fengkuangfantu/activity/FeedbackActivity.java
@@ -4,8 +4,12 @@ import com.xiaoding.fengkuangfantu.R;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -13,6 +17,11 @@ public class FeedbackActivity extends Activity {
 
     private TextView titleTextView;
     private ImageView returnImageView;
+    private EditText contentEditText;
+    private EditText contactEditText;
+    private Button mButton;
+    private String contentString;
+    private String contactString;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,5 +39,28 @@ public class FeedbackActivity extends Activity {
                 finish();
             }
         });
+        contentEditText = (EditText) findViewById(R.id.feedback_content);
+        contactEditText = (EditText) findViewById(R.id.feedback_contact);
+        mButton = (Button) findViewById(R.id.send);
+        mButton.setOnClickListener(new OnClickListener() {
+            
+            @Override
+            public void onClick(View arg0) {
+                // TODO Auto-generated method stub
+                SendMessageToUs();
+            }
+        });
+    }
+
+    private void SendMessageToUs() {
+        // TODO Auto-generated method stub
+        contentString = contentEditText.getText().toString();
+        contactString = contactEditText.getText().toString();
+        if (!TextUtils.isEmpty(contentString)) {
+            contentEditText.setText("");
+        }
+        if (!TextUtils.isEmpty(contactString)) {
+            contactEditText.setText("");
+        }
     }
 }
